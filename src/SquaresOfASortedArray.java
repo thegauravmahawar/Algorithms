b35b9ffbc94e3ce1ec21530fdef437ba602eb088
Gaurav Mahawar <thegauravmahawar@gmail.com>
2022-11-02 23:20:35 +0530

    Leetcode | Squares of a Sorted Array

--- src/SquaresOfASortedArray.java
+++ src/SquaresOfASortedArray.java
@@ -1,15 +1,32 @@
+import java.util.Arrays;
+
 public class SquaresOfASortedArray {
 
     public static void main(String... args) {
-
+        System.out.println(Arrays.toString(sortedSquares(new int[]{-4, -1, 0, 3, 10})));
+        System.out.println(Arrays.toString(sortedSquares(new int[]{-7, -3, 2, 3, 11})));
     }
 
     /**
-     *
      * @param nums
      * @return
      */
     private static int[] sortedSquares(int[] nums) {
-        return null;
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0;
+        int right = n - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            int value;
+            if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+                value = nums[right];
+                right--;
+            } else {
+                value = nums[left];
+                left++;
+            }
+            result[i] = value * value;
+        }
+        return result;
     }
 }
