98936e71e2c6927f284da8458a5c35a19b95daa5
Gaurav Mahawar <thegauravmahawar@gmail.com>
2023-02-27 18:11:09 +0530

    Linked List Cycle

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,40 @@
+/**
+ * @author Gaurav Mahawar
+ * @see <a href="https://leetcode.com/problems/linked-list-cycle/">Linked List Cycle</a>
+ */
+public class LinkedListCycle {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String... args) {
+
+    }
+
+    private static boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+
+        return false;
+    }
+}
