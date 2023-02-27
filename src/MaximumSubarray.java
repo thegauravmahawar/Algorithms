76dc9aa77683a970d347e7f70189b3b9500faa65
Gaurav Mahawar <thegauravmahawar@gmail.com>
2023-02-27 22:49:38 +0530

    Leetcode - Maximum Subarray

---
+++ src/MaximumSubarray.java
@@ -0,0 +1,22 @@
+/**
+ * @author Gaurav Mahawar
+ * @see <a href="https://leetcode.com/problems/maximum-subarray/">Maximum Subarray</a>
+ */
+public class MaximumSubarray {
+
+    public static void main(String... args) {
+
+    }
+
+    private static int maxSubArray(int[] nums) {
+        int maxSum = 0;
+        int subArraySum = 0;
+
+        for (int num : nums) {
+            subArraySum = Math.max(subArraySum + num, num);
+            maxSum = Math.max(subArraySum, maxSum);
+        }
+
+        return maxSum;
+    }
+}
