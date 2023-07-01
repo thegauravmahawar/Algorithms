78c3f6ece28b975b72d5bd861cf4176416c37e09
Gaurav Mahawar <thegauravmahawar@gmail.com>
2023-07-02 00:26:53 +0530

    Refactoring

--- algorithms-go/two-sum.go
+++ algorithms-go/two-sum.go
@@ -5,18 +5,18 @@ package main
 import "fmt"
 
 func twoSum(nums []int, target int) []int {
-	cache := make(map[int]int)
-	for index, num := range nums {
-		idx1, prs := cache[num]
-		if prs {
-			return []int{idx1, index}
-		} else {
-			cache[target-num] = index
-		}
-	}
-	return []int{}
+    cache := make(map[int]int)
+    for index, num := range nums {
+        idx1, prs := cache[num]
+        if prs {
+            return []int{idx1, index}
+        } else {
+            cache[target-num] = index
+        }
+    }
+    return []int{}
 }
 
 func main() {
-	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+    fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
 }
